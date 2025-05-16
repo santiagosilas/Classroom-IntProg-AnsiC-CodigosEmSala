b5b3a41aee80a17e12c789ef4b29dc0abb032a7d
Silas Santiago <santiagosilas@outlook.com>
2025-05-16 05:56:47 -0300

    .

--- q27v20250515.c
+++ q27v20250515.c
@@ -8,7 +8,7 @@ int main(){
     printf("a:");fgets(a, sizeof(a), stdin);
     printf("b:");fgets(b, sizeof(b), stdin);
     for(tamA = 0;a[tamA]!='\n';tamA++) c[tamA] = a[tamA];
-    c[tamA] = '-';
+    c[tamA] = ' ';
 	tamA = tamA + 1;
     for(tamB = 0;b[tamB]!='\n';tamB++) c[tamB+tamA] = b[tamB];
     c[tamB+tamA] = '\0';
